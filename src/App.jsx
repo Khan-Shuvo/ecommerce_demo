9ad9d0e7704bf55d5457592af8d160e0dc0233f7
khan-shuvo <shuvo2004@outlook.com>
2025-11-16 13:56:42 +0600

    replace browger router to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import TopHeader from "./components/navigatiion/TopHeader";
 import Navbar from "./components/navigatiion/Navbar";
 
